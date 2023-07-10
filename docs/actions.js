e7259a169774c658e36d4a2c4f7a2ec77051f663
renebec <39104197+renebec@users.noreply.github.com>
2023-07-10 02:48:44 -0600

    Add files via upload

--- docs/actions.js
+++ docs/actions.js
@@ -13,3 +13,19 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+
+function myFunction2() {
+  var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("more2");
+  var btnText2 = document.getElementById("myBtn2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnText2.innerHTML = "Leer más";
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnText2.innerHTML = "Mostrar menos";
+    moreText2.style.display = "inline";
+  }
+}
